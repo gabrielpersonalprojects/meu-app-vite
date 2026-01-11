7463b9e45748c9b08e0f512b12b8ee2479ad2755
gabrielpersonalprojects <gabrielpersonalproject1@gmail.com>
2026-01-11 01:06:42 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,10 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-3xl font-bold text-blue-600">✅ FUNCIONANDO!</h1>
+    </div>
+  )
+}
+
+export default App
 
